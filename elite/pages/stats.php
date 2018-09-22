ded15764421689251f740f4110b7d25889c1fe82
Marwolf <cleako@gmail.com>
2018-09-22 12:48:47 -0400

    Improved display of stats page

--- elite/pages/stats.php
+++ elite/pages/stats.php
@@ -1,7 +1,6 @@
 <div class="main">
     <div class="content">
         <article>
-            <div class="ranking">
                 <b><?php echo uniquePlayers(); ?></b> people have created <b><?php echo totalPlayers(); ?></b> players.<br />
                 The highest combat level is <b><?php echo topcombat(); ?>.</b><br />
                 <b><?php echo combat30(); ?></b> players are over combat level 30.<br />
@@ -20,7 +19,6 @@
                 <b><?php echo gold1m(); ?></b> players have over 1M gp.<br />
                 <b><?php echo gold12m(); ?></b> players have over 1.2M gp.<br />
                 <b><?php echo gold15m(); ?></b> players have over 1.5M gp.
-            </div>
         </article>
     </div>
 </div>
