4221bf7fd61b7615c037ba1e26203806ae0d7fa1
Marwolf <cleako@gmail.com>
2018-09-30 16:21:47 -0400

    Layout improvements to pages

--- elite/pages/stats.php
+++ elite/pages/stats.php
@@ -12,6 +12,7 @@ if(!defined('IN_PHPBB'))
             </headerbar>
         </div>
         <article>
+            <div style="margin-left: 80px; margin-right: 80px; margin-top: 45px; margin-bottom: 45px; color: lightgrey;" align="center">
                 <b><?php echo uniquePlayers(); ?></b> people have created <b><?php echo totalPlayers(); ?></b> players.<br />
                 The highest combat level is <b><?php echo topcombat(); ?>.</b><br />
                 <b><?php echo combat30(); ?></b> players are over combat level 30.<br />
@@ -29,7 +30,8 @@ if(!defined('IN_PHPBB'))
                 <b><?php echo gold400(); ?></b> players have over 400K gp.<br />
                 <b><?php echo gold1m(); ?></b> players have over 1M gp.<br />
                 <b><?php echo gold12m(); ?></b> players have over 1.2M gp.<br />
-                <b><?php echo gold15m(); ?></b> players have over 1.5M gp.
+                <b><?php echo gold15m(); ?></b> players have over 1.5M gp.<br /><br />
+            </div>
         </article>
     </div>
 </div>
