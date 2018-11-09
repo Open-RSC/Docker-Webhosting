70a69fc1b20fa86b2bbfa2593952fbef592aa199
Christopher Snow <csnow@live.ca>
2018-11-09 18:45:53 -0500

    Remove discord rel.

--- index.php
+++ index.php
@@ -22,8 +22,6 @@
     <link rel="stylesheet" href="/js/fancybox/jquery.fancybox-1.3.4.css" type="text/css" media="screen"/>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Exo">
 
-    <link rel="import" href="inc/discord.html">
-
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js" type="text/javascript"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.8.4/moment.min.js" type="text/javascript"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.4.3/jquery.min.js" type="text/javascript"></script>
