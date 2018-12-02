bea618b65486b3812b4107a6e05db67c2b471f9b
Marwolf <cleako@gmail.com>
2018-12-01 22:35:19 -0500

    Adds required Laravel files and sets permissions

---
+++ openrsc-web/bootstrap/cache/packages.php
@@ -0,0 +1,164 @@
+<?php return array (
+  'barryvdh/laravel-debugbar' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Barryvdh\\Debugbar\\ServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Debugbar' => 'Barryvdh\\Debugbar\\Facade',
+    ),
+  ),
+  'creativeorange/gravatar' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Creativeorange\\Gravatar\\GravatarServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Gravatar' => 'Creativeorange\\Gravatar\\Facades\\Gravatar',
+    ),
+  ),
+  'fideloper/proxy' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
+    ),
+  ),
+  'intervention/image' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Intervention\\Image\\ImageServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Image' => 'Intervention\\Image\\Facades\\Image',
+    ),
+  ),
+  'jaybizzle/laravel-crawler-detect' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Jaybizzle\\LaravelCrawlerDetect\\LaravelCrawlerDetectServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Crawler' => 'Jaybizzle\\LaravelCrawlerDetect\\Facades\\LaravelCrawlerDetect',
+    ),
+  ),
+  'jeremykenedy/laravel-exception-notifier' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'jeremykenedy\\laravelexceptionnotifier\\LaravelExceptionNotifier',
+    ),
+  ),
+  'jeremykenedy/laravel-https' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'jeremykenedy\\LaravelHttps\\LaravelHttpsServiceProvider',
+    ),
+  ),
+  'jeremykenedy/laravel-logger' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'jeremykenedy\\LaravelLogger\\LaravelLoggerServiceProvider',
+    ),
+  ),
+  'jeremykenedy/laravel-phpinfo' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'jeremykenedy\\LaravelPhpInfo\\LaravelPhpInfoServiceProvider',
+    ),
+  ),
+  'jeremykenedy/laravel-roles' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'jeremykenedy\\LaravelRoles\\RolesServiceProvider',
+    ),
+  ),
+  'jeremykenedy/laravel2step' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'jeremykenedy\\laravel2step\\laravel2stepServiceProvider',
+    ),
+  ),
+  'jeremykenedy/uuid' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'jeremykenedy\\Uuid\\UuidServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Uuid' => 'jeremykenedy\\Uuid\\Uuid',
+    ),
+  ),
+  'laravel/socialite' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Socialite\\SocialiteServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Socialite' => 'Laravel\\Socialite\\Facades\\Socialite',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'laravelcollective/html' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Collective\\Html\\HtmlServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Form' => 'Collective\\Html\\FormFacade',
+      'Html' => 'Collective\\Html\\HtmlFacade',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'rap2hpoutre/laravel-log-viewer' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Rap2hpoutre\\LaravelLogViewer\\LaravelLogViewerServiceProvider',
+    ),
+  ),
+  'socialiteproviders/manager' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'SocialiteProviders\\Manager\\ServiceProvider',
+    ),
+  ),
+);
