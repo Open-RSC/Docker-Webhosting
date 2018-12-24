9bb834f20f0efcfcd2729371849905be0f4527a4
Marwolf <cleako@gmail.com>
2018-12-24 00:13:55 -0500

    Adds sticky footer

--- new/evolution.php
+++ new/evolution.php
@@ -2,212 +2,206 @@
 <html lang="en">
 
 <head>
-    <link rel="import" href="header.html">
-    <title>Open RSC</title>
+	<link rel="import" href="header.html">
+	<title>Open RSC</title>
 </head>
 
 <body id="page-top">
 
 <!-- Navigation -->
 <nav class="navbar navbar-expand-lg fixed-top" id="mainNav">
-    <div class="container">
-        <!--<a class="navbar-brand js-scroll-trigger" href="#page-top">Open RSC</a>-->
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
-                data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
-                aria-label="Toggle navigation">
-            Menu
-            <i class="fas fa-bars"></i>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <a class="nav-link js-scroll-trigger" href="#home">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link js-scroll-trigger" href="#chat">Chat Log</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link js-scroll-trigger" href="#highscores">Highscores</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link js-scroll-trigger" href="#worldmap">Live Map</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link js-scroll-trigger" href="#events">Events</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link js-scroll-trigger" href="#information">Information</a>
-                </li>
-            </ul>
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link js-scroll-trigger" href="#login">Login</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link js-scroll-trigger" href="#register">Register</a>
-                </li>
-            </ul>
-        </div>
-    </div>
+	<div class="container">
+		<!--<a class="navbar-brand js-scroll-trigger" href="#page-top">Open RSC</a>-->
+		<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+				data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
+				aria-label="Toggle navigation">
+			Menu
+			<i class="fas fa-bars"></i>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarResponsive">
+			<ul class="navbar-nav mr-auto">
+				<li class="nav-item">
+					<a class="nav-link js-scroll-trigger" href="#home">Home</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link js-scroll-trigger" href="#chat">Chat Log</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link js-scroll-trigger" href="#highscores">Highscores</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link js-scroll-trigger" href="#worldmap">Live Map</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link js-scroll-trigger" href="#events">Events</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link js-scroll-trigger" href="#information">Information</a>
+				</li>
+			</ul>
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item">
+					<a class="nav-link js-scroll-trigger" href="#login">Login</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link js-scroll-trigger" href="#register">Register</a>
+				</li>
+			</ul>
+		</div>
+	</div>
 </nav>
 
 <!-- Title Section -->
 <section id="home" class="about-section text-center">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-8 mx-auto">
-                <img src="img/logo.png" height="300px" width="300px">
-                <h2 class="display-3 font-italic text-white mb-0">Evolution</h2>
-                <p class="text-white">Daring To Imagine An Entirely New Direction</p>
-            </div>
-        </div>
-        <img class="android" src="img/android.png" class="img-fluid" height="300px" width="600px;">
-    </div>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-8 mx-auto">
+				<img src="img/logo.png" height="300px" width="300px">
+				<h2 class="display-3 font-italic text-white mb-0">Evolution</h2>
+				<p class="text-white">Daring To Imagine An Entirely New Direction</p>
+			</div>
+		</div>
+		<img class="android" src="img/android.png" class="img-fluid" height="300px" width="600px;">
+	</div>
+	<!-- Footer -->
+	<div class="bg-black fixed-bottom social d-flex justify-content-center">
+		<a href="#" class="mx-2">
+			<i class="fab fa-reddit"></i>
+		</a>
+		<a href="#" class="mx-2">
+			<i class="fab fa-discord"></i>
+		</a>
+		<a href="#" class="mx-2">
+			<i class="fab fa-github"></i>
+		</a>
+	</div>
 </section>
+
 
 <!-- Projects Section -->
 <section id="projects" class="projects-section bg-light">
-    <div class="container">
+	<div class="container">
 
-        <!-- Featured Project Row -->
-        <div class="row align-items-center no-gutters mb-4 mb-lg-5">
-            <div class="col-xl-8 col-lg-7">
-                <img class="img-fluid mb-3 mb-lg-0" src="img/bg-masthead.jpg" alt="">
-            </div>
-            <div class="col-xl-4 col-lg-5">
-                <div class="featured-text text-center text-lg-left">
-                    <h4>Shoreline</h4>
-                    <p class="text-black-50 mb-0">Grayscale is open source and MIT licensed. This means you can use it
-                        for any project - even commercial projects! Download it, customize it, and publish your
-                        website!</p>
-                </div>
-            </div>
-        </div>
+		<!-- Featured Project Row -->
+		<div class="row align-items-center no-gutters mb-4 mb-lg-5">
+			<div class="col-xl-8 col-lg-7">
+				<img class="img-fluid mb-3 mb-lg-0" src="img/bg-masthead.jpg" alt="">
+			</div>
+			<div class="col-xl-4 col-lg-5">
+				<div class="featured-text text-center text-lg-left">
+					<h4>Shoreline</h4>
+					<p class="text-black-50 mb-0">Grayscale is open source and MIT licensed. This means you can use it
+						for any project - even commercial projects! Download it, customize it, and publish your
+						website!</p>
+				</div>
+			</div>
+		</div>
 
-        <!-- Project One Row -->
-        <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
-            <div class="col-lg-6">
-                <img class="img-fluid" src="img/demo-image-01.jpg" alt="">
-            </div>
-            <div class="col-lg-6">
-                <div class="bg-black text-center h-100 project">
-                    <div class="d-flex h-100">
-                        <div class="project-text w-100 my-auto text-center text-lg-left">
-                            <h4 class="text-white">Misty</h4>
-                            <p class="mb-0 text-white-50">An example of where you can put an image of a project, or
-                                anything else, along with a description.</p>
-                            <hr class="d-none d-lg-block mb-0 ml-0">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+		<!-- Project One Row -->
+		<div class="row justify-content-center no-gutters mb-5 mb-lg-0">
+			<div class="col-lg-6">
+				<img class="img-fluid" src="img/demo-image-01.jpg" alt="">
+			</div>
+			<div class="col-lg-6">
+				<div class="bg-black text-center h-100 project">
+					<div class="d-flex h-100">
+						<div class="project-text w-100 my-auto text-center text-lg-left">
+							<h4 class="text-white">Misty</h4>
+							<p class="mb-0 text-white-50">An example of where you can put an image of a project, or
+								anything else, along with a description.</p>
+							<hr class="d-none d-lg-block mb-0 ml-0">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 
-        <!-- Project Two Row -->
-        <div class="row justify-content-center no-gutters">
-            <div class="col-lg-6">
-                <img class="img-fluid" src="img/demo-image-02.jpg" alt="">
-            </div>
-            <div class="col-lg-6 order-lg-first">
-                <div class="bg-black text-center h-100 project">
-                    <div class="d-flex h-100">
-                        <div class="project-text w-100 my-auto text-center text-lg-right">
-                            <h4 class="text-white">Mountains</h4>
-                            <p class="mb-0 text-white-50">Another example of a project with its respective description.
-                                These sections work well responsively as well, try this theme on a small screen!</p>
-                            <hr class="d-none d-lg-block mb-0 mr-0">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+		<!-- Project Two Row -->
+		<div class="row justify-content-center no-gutters">
+			<div class="col-lg-6">
+				<img class="img-fluid" src="img/demo-image-02.jpg" alt="">
+			</div>
+			<div class="col-lg-6 order-lg-first">
+				<div class="bg-black text-center h-100 project">
+					<div class="d-flex h-100">
+						<div class="project-text w-100 my-auto text-center text-lg-right">
+							<h4 class="text-white">Mountains</h4>
+							<p class="mb-0 text-white-50">Another example of a project with its respective description.
+								These sections work well responsively as well, try this theme on a small screen!</p>
+							<hr class="d-none d-lg-block mb-0 mr-0">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 
-    </div>
+	</div>
 </section>
 
 <!-- Signup Section -->
 <section id="signup" class="signup-section">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-10 col-lg-8 mx-auto text-center">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-10 col-lg-8 mx-auto text-center">
 
-                <i class="far fa-paper-plane fa-2x mb-2 text-white"></i>
-                <h2 class="text-white mb-5">Subscribe to receive updates!</h2>
+				<i class="far fa-paper-plane fa-2x mb-2 text-white"></i>
+				<h2 class="text-white mb-5">Subscribe to receive updates!</h2>
 
-                <form class="form-inline d-flex">
-                    <input type="email" class="form-control flex-fill mr-0 mr-sm-2 mb-3 mb-sm-0" id="inputEmail"
-                           placeholder="Enter email address...">
-                    <button type="submit" class="btn btn-primary mx-auto">Subscribe</button>
-                </form>
+				<form class="form-inline d-flex">
+					<input type="email" class="form-control flex-fill mr-0 mr-sm-2 mb-3 mb-sm-0" id="inputEmail"
+						   placeholder="Enter email address...">
+					<button type="submit" class="btn btn-primary mx-auto">Subscribe</button>
+				</form>
 
-            </div>
-        </div>
-    </div>
+			</div>
+		</div>
+	</div>
 </section>
 
 <!-- Contact Section -->
 <section class="contact-section bg-black">
-    <div class="container">
+	<div class="container">
 
-        <div class="row">
+		<div class="row">
 
-            <div class="col-md-4 mb-3 mb-md-0">
-                <div class="card py-4 h-100">
-                    <div class="card-body text-center">
-                        <i class="fas fa-map-marked-alt text-primary mb-2"></i>
-                        <h4 class="text-uppercase m-0">Address</h4>
-                        <hr class="my-4">
-                        <div class="small text-black-50">4923 Market Street, Orlando FL</div>
-                    </div>
-                </div>
-            </div>
+			<div class="col-md-4 mb-3 mb-md-0">
+				<div class="card py-4 h-100">
+					<div class="card-body text-center">
+						<i class="fas fa-map-marked-alt text-primary mb-2"></i>
+						<h4 class="text-uppercase m-0">Address</h4>
+						<hr class="my-4">
+						<div class="small text-black-50">4923 Market Street, Orlando FL</div>
+					</div>
+				</div>
+			</div>
 
-            <div class="col-md-4 mb-3 mb-md-0">
-                <div class="card py-4 h-100">
-                    <div class="card-body text-center">
-                        <i class="fas fa-envelope text-primary mb-2"></i>
-                        <h4 class="text-uppercase m-0">Email</h4>
-                        <hr class="my-4">
-                        <div class="small text-black-50">
-                            <a href="#">hello@yourdomain.com</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+			<div class="col-md-4 mb-3 mb-md-0">
+				<div class="card py-4 h-100">
+					<div class="card-body text-center">
+						<i class="fas fa-envelope text-primary mb-2"></i>
+						<h4 class="text-uppercase m-0">Email</h4>
+						<hr class="my-4">
+						<div class="small text-black-50">
+							<a href="#">hello@yourdomain.com</a>
+						</div>
+					</div>
+				</div>
+			</div>
 
-            <div class="col-md-4 mb-3 mb-md-0">
-                <div class="card py-4 h-100">
-                    <div class="card-body text-center">
-                        <i class="fas fa-mobile-alt text-primary mb-2"></i>
-                        <h4 class="text-uppercase m-0">Phone</h4>
-                        <hr class="my-4">
-                        <div class="small text-black-50">+1 (555) 902-8832</div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="social d-flex justify-content-center">
-            <a href="#" class="mx-2">
-                <i class="fab fa-reddit"></i>
-            </a>
-            <a href="#" class="mx-2">
-                <i class="fab fa-discord"></i>
-            </a>
-            <a href="#" class="mx-2">
-                <i class="fab fa-github"></i>
-            </a>
-        </div>
-
-    </div>
+			<div class="col-md-4 mb-3 mb-md-0">
+				<div class="card py-4 h-100">
+					<div class="card-body text-center">
+						<i class="fas fa-mobile-alt text-primary mb-2"></i>
+						<h4 class="text-uppercase m-0">Phone</h4>
+						<hr class="my-4">
+						<div class="small text-black-50">+1 (555) 902-8832</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </section>
 
-<!-- Footer -->
-<footer class="bg-black small text-center text-white-50">
-    <div class="container">
-        Copyright &copy; Your Website 2018
-    </div>
-</footer>
 
 <!-- Bootstrap core JavaScript -->
 <script src="vendor/jquery/jquery.min.js"></script>
@@ -220,5 +214,4 @@
 <script src="js/grayscale.min.js"></script>
 
 </body>
-
 </html>
