c043383ba7ff6919284d1faef52a4b4c917b8e8b
Marwolf <cleako@gmail.com>
2019-01-29 19:55:45 -0500

    Removal of old files, creation of quest listing page, nav updates

---
+++ pages/quest.php
@@ -0,0 +1,47 @@
+<?php
+if (!defined('IN_SITE')) {
+	die("You do not have permission to access this file.");
+}
+?>
+
+<article class="text-info table-dark spaced-body full-width full-height">
+	<div class="container border-left border-info border-right table-wrapper-scroll-y mCustomScrollbar"
+		 data-mcs-theme="minimal">
+		<h2 class="h2 text-center text-capitalize display-3 pb-4">Quest Guide</h2>
+		<div class="row sm-stats" style="text-transform: unset;">
+			<div class="text-primary">
+				<div class="flex-row">
+					<div class="pl-3 pr-3 container">
+
+						<div class="stats pl-5 pr-5">
+							<div class="h4 text-info">F2P Quests</div>
+							<div class="flex-row">
+								<div class="sm-stats">
+								<span class="d-block"><span class="text-info">Shield of Arrav</span> Medium</span>
+								</div>
+								<div class="sm-stats">
+									<span class="d-sm-block"><span class="text-info">Items needed:</span> 20gp or food and a weapon</span>
+									<span class="d-sm-block">or Weaponmaster (level 25, if you join the Black Arm Gang)</span>
+								</div>
+							</div>
+						</div>
+
+						<div class="stats pl-5 pr-5">
+							<div class="h4 text-info">Members Quests</div>
+							<div class="flex-row">
+								<div class="sm-stats">
+									<span class="d-block"><span class="text-info">Shield of Arrav</span> Medium</span>
+								</div>
+								<div class="sm-stats">
+									<span class="d-sm-block"><span class="text-info">Items needed:</span> 20gp or food and a weapon</span>
+									<span class="d-sm-block">or Weaponmaster (level 25, if you join the Black Arm Gang)</span>
+								</div>
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</article>
