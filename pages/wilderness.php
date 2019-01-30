554573ecd0c7bd588ea7abf631a838a4af2b362e
Marwolf <cleako@gmail.com>
2019-01-30 00:08:56 -0500

    Updates worldmap, navbar style changes

--- pages/wilderness.php
+++ pages/wilderness.php
@@ -8,7 +8,7 @@ define('IN_SITE', true);
 
 <article class="text-info table-dark spaced-body full-width text-center">
 	<div class="container-fluid table-wrapper-scroll-y mCustomScrollbar"
-		 data-mcs-theme="minimal">
-		<img src="/img/wildmapcl.gif">
+		 data-mcs-axis="yx">
+		<img src="/img/Wilderness.png">
 	</div>
 </article>
