6eccb3050f479677d34e073561691c0cb7fbd619
Marwolf <cleako@gmail.com>
2019-02-07 18:23:14 -0500

    Adds black knights fortress quest page

---
+++ pages/quest-black-knights-fortress.php
@@ -0,0 +1,159 @@
+<?php
+if (!defined('IN_SITE')) {
+	die("You do not have permission to access this file.");
+}
+?>
+
+<article class="text-info table-dark spaced-body full-width full-height">
+	<div class="container border-left border-info border-right table-wrapper-scroll-y mCustomScrollbar"
+		 data-mcs-theme="minimal">
+		<h2 class="h2 text-center text-capitalize display-3 pb-4">Black Knight's Fortress</h2>
+		<div class="row sm-stats justify-content-center" style="text-transform: unset;">
+			<div class="text-primary">
+				<div class="row justify-content-center">
+					<div class="pl-3 pr-3 container">
+						<div class="pb-1" align="center">
+							<img class="display-glow img-fluid"
+								 src="../img/quests/Black_Knight_Quest_Complete.png">
+						</div>
+
+						<div class="flex-row stats pl-5 pr-5">
+							<div class="sm-stats col-sm">
+								<span class="d-block"><span class="text-info">Release date:</span> 6 April 2001</span>
+								<span class="d-block"><span class="text-info">Start:</span> Talk to Sir Amik Varze in the White Knights Castle</span>
+								<span class="d-block"><span class="text-info">Difficulty:</span> <span
+										class="small text-info font-weight-bold">
+									<i class="fas fa-star"></i>
+									<i class="fas fa-star"></i>
+								</span><span
+										class="small">
+									<i class="far fa-star"></i>
+									<i class="far fa-star"></i>
+									<i class="far fa-star"></i>
+									</span></span>
+								<span class="d-block"><span class="text-info">Length:</span> Medium</span>
+
+								<span class="d-block d-responsive"><span class="text-info">Items needed:</span>
+									<li>- Medium Bronze Helmet (Can be bought from Peksa's Helmet Shop in Barbarian Village)</li>
+									<li>- Iron Chain Mail Body (Can be bought from Horvik's Armour Shop in Varrock, or from Wayne's Chains in Falador)</li>
+									<li>- Cabbage (One that is not from the patch near Draynor Manor)</li>
+								</span>
+								<span class="d-block"><span
+										class="text-info">Requirements:</span> 12 Quest Points</span>
+								<span class="d-block"><span class="text-info">Monsters to kill:</span> 3 Black Knights (level 46) unless you can rapidly click your way through the door before they kill/swarm you</span>
+							</div>
+						</div>
+
+						<div class="stats pl-5 pr-5" id="accordion">
+							<div class="h4 text-info">Walkthrough</div>
+							<div class="text-primary btn btn-link" data-toggle="collapse"
+								 data-target="#collapsewalkthrough"
+								 aria-expanded="false" aria-controls="collapseOne">
+								Show contents
+							</div>
+							<div id="collapsewalkthrough" class="pt-3 collapse hide" aria-labelledby="headingOne"
+								 data-parent="#accordion">
+								<img class="pb-2 img-fluid" src="../img/quests/Sir_Amik_Varze.png">
+								<span class="d-block">
+								To start this quest, talk to Sir Amik Varze who is found wandering on the second floor of the White Knights Castle in Falador. Tell him you are seeking a quest and he'll say he needs some spy work done. The Black Knights at the Black Knights' Fortress are threatening to invade Falador with a secret weapon, and Sir Amik Varze wants you to find out what it is and sabotage it.</span>
+							</div>
+						</div>
+
+						<div class="stats pl-5 pr-5" id="accordion">
+							<div class="h4 text-info">Gathering the materials</div>
+							<div class="text-primary btn btn-link" data-toggle="collapse" data-target="#collapsephoenix"
+								 aria-expanded="false" aria-controls="collapseOne">
+								Show contents
+							</div>
+							<div id="collapsephoenix" class="pt-3 collapse hide" aria-labelledby="headingOne"
+								 data-parent="#accordion">
+								<img class="pb-2 img-fluid" src="../img/quests/Cabbage_patch.png">
+								<span class="d-block">Before heading to the Black Knights' Fortress, you'll need a cabbage, Medium Bronze Helmet, and an Iron Chain Mail Body.</span>
+
+								<span class="pt-3 d-block">Cabbage may be picked from fields near Falador, Lumbridge, or near the Monastery. Do not pick up cabbage from Draynor Manor as this cabbage will not work in the quest. You can buy a Medium Bronze Helmet from Peksa in Barbarian Village and an Iron Chain Mail Body from Horvik the Armourer in Varrock or Wayne in Falador.</span>
+
+								<span class="pt-3 d-block">Once you have these three items, head to the Black Knights' Fortress, located on the north side of Ice Mountain and west of the Monastery.</span>
+							</div>
+						</div>
+
+						<div class="stats pl-5 pr-5" id="accordion">
+							<div class="h4 text-info">Infiltrating the fortress</div>
+							<div class="text-primary btn btn-link" data-toggle="collapse"
+								 data-target="#collapseblackarm"
+								 aria-expanded="false" aria-controls="collapseOne">
+								Show contents
+							</div>
+							<div id="collapseblackarm" class="pt-3 collapse hide" aria-labelledby="headingOne"
+								 data-parent="#accordion">
+								<span class="d-block">Equip your Medium Bronze Helmet and Iron Chain Mail Body and walk right in through the eastern door of the fortress. The guards won't let you in the eastern door if these items are not equipped.</span>
+
+								<span class="pt-3 d-block">Once inside, go through the odd looking wall to the north and climb up the ladder.</span>
+
+								<img class="pt-2 pb-2 img-fluid" src="../img/quests/Black_Knight_Odd_looking_wall.png">
+
+								<span class="pt-3 d-block">In this room, you'll notice a grill to your west. Right-click it and select "listen" to eavesdrop on a conversation between a goblin named Greldo, a Witch, and a Black Knight. You'll hear them talking about an invincibility potion that they are making, and the missing the last ingredient, a cabbage from Draynor Manor. You learn that any other cabbage will destroy the potion, which is your goal.</span>
+
+								<img class="pt-2 pb-2 img-fluid" src="../img/quests/Black_Knights_Fortress_1.png">
+							</div>
+						</div>
+
+						<div class="stats pl-5 pr-5" id="accordion">
+							<h4 class="text-info">Sabotage</h4>
+							<div class="text-primary btn btn-link" data-toggle="collapse" data-target="#collapsefinale"
+								 aria-expanded="false" aria-controls="collapseOne">
+								Show contents
+							</div>
+							<div id="collapsefinale" class="pt-3 collapse hide" aria-labelledby="headingOne"
+								 data-parent="#accordion">
+
+								<span class="d-block">Climb back down the ladder, pass through the odd looking wall, and enter the room west of you with the three Black Knights. Unless you successfully go through the door by rapidly clicking the door which leads to the stairs, you will need to fight the three Black Knights to get through the room so bring a good weapon and armour. Sometimes it is only necessary to kill one knight. Potions and attacking with both Magic and Melee simultaneously is highly recommended if your character has a low Combat level. You can also get all three knights down to very little Hits and then try to open door.</span>
+
+								<img class="pt-2 pb-2 img-fluid" src="../img/quests/Black_Knight_hole.png">
+
+								<span class="pt-3 d-block">If you cannot rapidly click through the door you may have to kill all three of the knights in the room before the first one respawns in order to get through the door.</span>
+
+								<span class="pt-3 d-block">After making it through the door and going up the stairs, climb the ladder to the top floor. Go through the odd looking wall to the north, right click and use your cabbage on the hole. Be very careful not to eat the cabbage. You will hear the Witch "groan in dismay", revealing that the potion has been destroyed.</span>
+
+								<span class="pt-3 d-block">When you go to leave the fortress the Black knights will no longer be aggressive toward you.</span>
+
+								<span class="pt-3 d-block">Return to Falador and talk to Sir Amik Varze to receive your reward, completing the quest.</span>
+
+								<img class="pt-2 pb-2 img-fluid" src="../img/quests/Black_Knight_Quest_Complete.png">
+							</div>
+						</div>
+
+						<div class="stats pl-5 pr-5" id="accordion">
+							<div class="h4 text-info">Rewards</div>
+							<div class="text-primary btn btn-link" data-toggle="collapse" data-target="#collapserewards"
+								 aria-expanded="false" aria-controls="collapseOne">
+								Show contents
+							</div>
+							<div id="collapserewards" class="pt-3 collapse hide" aria-labelledby="headingOne"
+								 data-parent="#accordion">
+								<ul>
+									<li style="list-style: unset;">
+										<span class="text-success font-weight-bold">
+											3 Quest Points
+										</span>
+									</li>
+									<li style="list-style: unset;">
+										<div class="row">
+											<div class="pt-2 pb-2 display-glow item10"></div>
+											<span class="text-success font-weight-bold" style="padding-top: 12px;">2500 coins</span>
+										</div>
+									</li>
+								</ul>
+							</div>
+						</div>
+
+						<div class="text-center pt-5 pb-5">
+							<a href="https://classic.runescape.wiki/w/Black_Knights%27_Fortress_(quest)">Thank you to the Runescape
+								Classic Wiki for
+								this quest guide!</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</article>
