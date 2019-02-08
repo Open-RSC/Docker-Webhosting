eff7b6feb303f9b1a7aecc9365b0c483f2df603d
Marwolf <cleako@gmail.com>
2019-02-07 22:10:25 -0500

    Adds dragon slayer quest page

---
+++ pages/quest-dragon-slayer.php
@@ -0,0 +1,245 @@
+<?php
+if (!defined('IN_SITE')) {
+	die("You do not have permission to access this file.");
+}
+?>
+
+<article class="text-info table-dark spaced-body full-width full-height">
+	<div class="container border-left border-info border-right table-wrapper-scroll-y mCustomScrollbar"
+		 data-mcs-theme="minimal">
+		<h2 class="h2 text-center text-capitalize display-3 pb-4">Dragon Slayer</h2>
+		<div class="row sm-stats justify-content-center" style="text-transform: unset;">
+			<div class="text-primary">
+				<div class="row justify-content-center">
+					<div class="pl-3 pr-3 container">
+						<div class="pb-1" align="center">
+							<img class="display-glow img-fluid"
+								 src="../img/quests/Dragon_Slayer_completed.png">
+						</div>
+
+						<div class="flex-row stats pl-5 pr-5">
+							<div class="sm-stats col-sm">
+								<span class="d-block"><span
+										class="text-info">Release date:</span> 23 September 2001</span>
+								<span class="d-block"><span class="text-info">Start:</span> Speak to the Guildmaster inside the Champions' Guild.</span>
+								<span class="d-block"><span class="text-info">Difficulty:</span> <span
+										class="small text-info font-weight-bold">
+									<i class="fas fa-star"></i>
+									<i class="fas fa-star"></i>
+									<i class="fas fa-star"></i>
+									<i class="fas fa-star"></i>
+								</span><span
+										class="small">
+									<i class="far fa-star"></i>
+									</span></span>
+								<span class="d-block"><span class="text-info">Length:</span> Long</span>
+
+								<span class="d-block d-responsive"><span class="text-info">Items needed:</span>
+									<li>- 2000 coins</li>
+									<li>- Three planks (found in the Wilderness, or at the Barbarian Outpost)</li>
+									<li>- 12 nails (6 bars worth)</li>
+									<li>- A hammer</li>
+									<li>- An unfired bowl</li>
+									<li>- A Wizard's mind bomb</li>
+									<li>- A lobster pot</li>
+									<li>- Silk</li>
+									<li>- An anti dragon breath shield (obtainable from the Duke of Lumbridge after speaking to Oziach)</li>
+									<li>- A one-handed weapon with which to kill Elvarg</li>
+									<li>- Armour (Adamant armour or better is recommended)</li>
+									<li>- A law-rune and an air-rune</li>
+									<li>- Food</li>
+									<li>- Protect Items (optional)</li>
+									<li>- Paralyze Monster (optional)</li>
+								</span>
+								<span class="d-block d-responsive"><span class="text-info">Requirements:</span>
+									<li>- 32 Quest Points</li>
+									<li>- 33 Magic for Telekinetic Grab (boosts such as Wizard's mind bomb can be used)</li>
+									<li>- 34 Smithing to create 12 Nails (6 steel bars worth) if you cannot find a player to trade you some</li>
+									<li>- The ability to defeat the level 110 dragon Elvarg that uses a dragon fire attack that hits up to 50 damage without using an anti dragon breath shield</li>
+									<li>- The ability to defeat Melzar the Mad (level 43)</li>
+									<li>- The ability to defeat a level 79 lesser demon (use of Silverlight may help lower leveled players)</li>
+								</span>
+								<span class="d-block d-responsive"><span class="text-info">Monsters to kill:</span>
+									<li>- Rats (level 3)</li>
+									<li>- Ghosts (level 19)</li>
+									<li>- Skeletons (level 22)</li>
+									<li>- Zombies (level 24)</li>
+									<li>- Melzar the Mad (level 43)</li>
+									<li>- Lesser demon (level 79)</li>
+									<li>- Elvarg (level 110)</li>
+								</span>
+							</div>
+						</div>
+
+						<div class="stats pl-5 pr-5" id="accordion">
+							<div class="h4 text-info">Walkthrough</div>
+							<div class="text-primary btn btn-link" data-toggle="collapse"
+								 data-target="#collapsewalkthrough"
+								 aria-expanded="false" aria-controls="collapseOne">
+								Show contents
+							</div>
+							<div id="collapsewalkthrough" class="pt-3 collapse hide" aria-labelledby="headingOne"
+								 data-parent="#accordion">
+								<img class="pb-2 img-fluid" src="../img/quests/440px-Dragon_slayer_start.png">
+								<span class="d-block">To start the quest, ask the Guildmaster in the Champions' Guild, located south west of Varrock, for a quest. He will direct you to Oziach, who can be found in a small house west of Edgeville.</span>
+								<span class="pt-3 d-block">Once you arrive at Oziach's house, ask him if you can purchase a Rune Plate Mail Body and he say that you must to first prove yourself as a hero to earn the right. To prove yourself, he wants you to kill the dragon named Elvarg on Crandor.</span>
+								<img class="pt-2 pb-2 img-fluid" src="../img/quests/Dragon_slayer_oziach.png">
+								<span class="pt-3 d-block">Ask where the dragon is located and he will explain on Crandor, but that nobody knows where the island is any more. Oziach then says that there was a map showing its location but it was torn into three pieces and scattered.</span>
+								<span class="pt-3 d-block">Ask for info on all three pieces to know their locations. When you're done, he will give you the Maze key to get the first map piece.</span>
+							</div>
+						</div>
+
+						<div class="stats pl-5 pr-5" id="accordion">
+							<div class="h4 text-info">First map piece</div>
+							<div class="text-primary btn btn-link" data-toggle="collapse" data-target="#collapsephoenix"
+								 aria-expanded="false" aria-controls="collapseOne">
+								Show contents
+							</div>
+							<div id="collapsephoenix" class="pt-3 collapse hide" aria-labelledby="headingOne"
+								 data-parent="#accordion">
+								<span class="d-block">Head to a bank in Falador and stock up for combat against several monsters including a level 79 Lesser demon. Use of Silverlight may help lower leveled players defeat this high leveled monster. Good food, full prayer points, and potions are all good items to bring if you have them.</span>
+								<img class="pt-2 pb-2 img-fluid" src="../img/quests/440px-Maze_entrance.png">
+								<span class="pt-3 d-block">Bring your maze key and go to Melzar's Maze, located just south of the Crafting Guild. Use your key on the door to enter.</span>
+								<span class="pt-3 d-block">Once inside, you must kill the Rats (level 13) until you find one that drops a Red key. Use it on the north-west door and head up the ladder. If you can't find the key, make sure you check the drops carefully and you're not killing the same rat over and over.</span>
+								<span class="pt-3 d-block">Now kill the Ghosts (level 25) until you find the one that drops the Orange Key. Go to the east wall and use it on the second door from the north and climb the ladder.</span>
+								<span class="pt-3 d-block">Kill the Skeletons (level 31) on this floor until you get a yellow key and use it on the south-western door. Follow the passageway and then go down a series of ladders to reach the basement.</span>
+								<span class="pt-3 d-block">Kill the Zombies (level 32) until you get a Blue key and use it to unlock the northern-most door with Melzar the Mad on the other side. Note that southern door is an emergency exit that will take you outside, requiring you to complete the maze again if you leave. Kill Melzar the Mad (level 45) and use the Magenta key he drops on the north door, leading to the Lesser Demon.</span>
+								<span class="pt-3 d-block">Note again that the western door here leads to the emergency exit. Kill the lesser demon (level 79) and use the black key it drops on the northern door leading to a small room with the wooden floors. Search the chest in this room to obtain the first map piece. When you have it, climb the ladder to exit the maze.</span>
+							</div>
+						</div>
+
+						<div class="stats pl-5 pr-5" id="accordion">
+							<div class="h4 text-info">Second map piece</div>
+							<div class="text-primary btn btn-link" data-toggle="collapse"
+								 data-target="#collapseblackarm"
+								 aria-expanded="false" aria-controls="collapseOne">
+								Show contents
+							</div>
+							<div id="collapseblackarm" class="pt-3 collapse hide" aria-labelledby="headingOne"
+								 data-parent="#accordion">
+								<span class="d-block">Before talking to the Oracle on Ice Mountain, have these four items in your inventory: a Wizard's Mind Bomb, silk, lobster pot, and an Unfired Bowl.</span>
+								<img class="pt-2 pb-2 img-fluid" src="../img/quests/440px-Meeting_Oracle.png">
+								<span class="pt-3 d-block">With the items listed above in your inventory, head to Ice Mountain (north of Falador, near the Monastery and Black Knights Fortress)</span>
+								<span class="pt-3 d-block">Go to the top of the mountain and speak with the Oracle. Tell her you seek a piece of a map of Crandor and she will give you a riddle:</span>
+								<blockquote class="pt-3 d-block blockquote">
+									"The map's behind a door below,
+									but entering is rather tough.
+									This is what you need to know,
+									You must have the following stuff:
+									First, a drink used by a mage,
+									Next, some worm string changed to a sheet,
+									Then, a small crustacean cage,
+									Last, a bowl that's not seen heat."
+								</blockquote>
+								<span class="pt-3 d-block">Make sure you have those four items in your inventory and head south to the building at the foot of the mountain</span>
+								<img class="pt-2 pb-2 img-fluid" src="../img/quests/440px-Dwarven_mine_door.png">
+								<span class="pt-3 d-block">Climb down the ladder in the house to the Dwarven Mines. Once you're in the mine follow the path east until you find a lone door. Opening it will remove the four items from your inventory.</span>
+								<span class="pt-3 d-block">Search the chest in this room and you will obtain the second map piece.</span>
+							</div>
+						</div>
+
+						<div class="stats pl-5 pr-5" id="accordion">
+							<h4 class="text-info">Third map piece</h4>
+							<div class="text-primary btn btn-link" data-toggle="collapse" data-target="#collapsefinale1"
+								 aria-expanded="false" aria-controls="collapseOne">
+								Show contents
+							</div>
+							<div id="collapsefinale1" class="pt-3 collapse hide" aria-labelledby="headingOne"
+								 data-parent="#accordion">
+								<img class="pb-2 img-fluid" src="../img/quests/Map_(item).png">
+								<span class="d-block">The third map piece is the least time consuming to get. Make sure you have the runes for Telekinetic grab (1 law-rune, 1 air-rune), and a Ranged weapon or runes to cast magic spells.</span>
+								<span class="pt-3 d-block">Head to Port Sarim and then enter the jail located in the south-west area of the port.</span>
+								<img class="pt-2 pb-2 img-fluid" src="../img/quests/440px-Wormbrain_location.png">
+								<span class="pt-3 d-block">You will see the goblin Wormbrain behind bars and a door that you cannot open. Kill him using magic or arrows, and then cast Telekinetic grab on the piece of map he drops.</span>
+								<span class="pt-3 d-block">Now that you have all three pieces, use one piece on another to create the full map of Crandor.</span>
+							</div>
+						</div>
+
+						<div class="stats pl-5 pr-5" id="accordion">
+							<h4 class="text-info">Purchasing the Lumbridge Lady</h4>
+							<div class="text-primary btn btn-link" data-toggle="collapse" data-target="#collapsefinale2"
+								 aria-expanded="false" aria-controls="collapseOne">
+								Show contents
+							</div>
+							<div id="collapsefinale2" class="pt-3 collapse hide" aria-labelledby="headingOne"
+								 data-parent="#accordion">
+								<span class="d-block">You have the map, now you need a ship. For this part you will need 2,000 coins, 12 nails (made from 6 steel bars, 34 smithing required), 3 planks, and a hammer.</span>
+								<img class="pt-2 pb-2 img-fluid" src="../img/quests/440px-Hole_in_Lady_Lum.png">
+								<span class="pt-3 d-block">Go to Port Sarim and talk to Klarense on the north western docks. He will say the boat nearby is his, the Lumbridge Lady, but that it is not currently seaworthy.</span>
+								<span
+									class="pt-3 d-block">Offer to buy it and Klarense will sell it for 2,000 coins.</span>
+								<span class="pt-3 d-block">Once you buy it, you have to repair a hole in the ship to make it seaworthy again. Board the boat and use the 3 planks on the damaged part of the hull to repair it. The 12 nails will be consumed 4 at a time, 3 for each plank.</span>
+								<img class="pt-2 pb-2 img-fluid" src="../img/quests/440px-Taling_to_Ned.png">
+								<span class="pt-3 d-block">Now that the ship is repaired, head to Draynor Village with the map and convince Ned to sail you to Crandor. Once he agrees, he says he will meet you on the ship at Port Sarim.</span>
+								<span class="pt-3 d-block">You are now ready to slay the dragon Elvarg!</span>
+							</div>
+						</div>
+
+						<div class="stats pl-5 pr-5" id="accordion">
+							<h4 class="text-info">Killing Elvarg</h4>
+							<div class="text-primary btn btn-link" data-toggle="collapse" data-target="#collapsefinale3"
+								 aria-expanded="false" aria-controls="collapseOne">
+								Show contents
+							</div>
+							<div id="collapsefinale3" class="pt-3 collapse hide" aria-labelledby="headingOne"
+								 data-parent="#accordion">
+								<img class="pt-2 pb-2 img-fluid" src="../img/quests/Talking_to_Duke.png">
+								<span class="d-block">All that is left to do now is kill Elvarg. Make sure to pick up an Anti dragon breath Shield from the Duke of Lumbridge, found on the upper floor of the castle in Lumbridge. You should bring the best armour and one-handed weapon you can use, along with the anti dragon breath shield, though you may bring another shield as well as you will be near aggressive mid-to-high level monsters. Any extra spaces in your inventory should be filled with lots of food and any potions or runes you would like to use. Some players choose to cast spells with magic while fighting with melee. Teleport runes are recommended if you want to avoid walking past skeletons and several Lesser Demons after the fight.</span>
+								<span class="pt-3 d-block">Make sure you have the map with you and then head to the Lumbridge Lady and board it.</span>
+								<span class="pt-3 d-block">Speak to Ned who will take you to Crandor, though doing so will damage the ship in the process, leaving you stranded on Crandor.</span>
+								<img class="pt-2 pb-2 img-fluid" src="../img/quests/440px-Arriving_at_Crandor.png">
+								<span class="pt-3 d-block">Once you arrive, leave the ship and make your way around the mountain to the peak, dodging aggressive Skeletons (level 31), Scorpions (level 26), Hobgoblins (level 32), and a singular Lesser Demon (level 79). Once you are at the top, go down the stairs to enter the cave under Crandor.</span>
+								<span class="pt-3 d-block">Once you are in the cave, you will be in a room with aggressive level 31 Skeletons. Continue past them and you will see level 54 Skeletons protecting Elvarg's lair. Make sure your anti dragon breath shield is equipped and open the door leading to Elvarg's lair. Be prepared for her to immediately start attacking you.</span>
+								<img class="pt-2 pb-2 img-fluid" src="../img/quests/440px-Fighting_Elvarg.png">
+								<span class="pt-3 d-block">Attack Elvarg (level 110) and keep an eye on your HP. The dragon breath attack will hit you constantly for small numbers and is unavoidable. Once your Hits drop to about half, run away and eat 1 or 2 pieces of food before resuming combat.</span>
+								<span class="pt-3 d-block">Once Elvarg is defeated, the quest is complete and you will be teleported back out in to the middle of the level 54 skeletons. If you are weakened from the fight it may be a good idea to teleport, otherwise, head past the skeletons and lesser demons and use the secret passage to exit to Karamja.</span>
+								<span class="pt-3 d-block">You can now return to Oziach and purchase a Rune Plate Mail Body for 65,000 coins.</span>
+								<img class="pt-2 pb-2 img-fluid" src="../img/quests/Dragon_Slayer_completed.png">
+							</div>
+						</div>
+
+						<div class="stats pl-5 pr-5" id="accordion">
+							<div class="h4 text-info">Rewards</div>
+							<div class="text-primary btn btn-link" data-toggle="collapse" data-target="#collapserewards"
+								 aria-expanded="false" aria-controls="collapseOne">
+								Show contents
+							</div>
+							<div id="collapserewards" class="pt-3 collapse hide" aria-labelledby="headingOne"
+								 data-parent="#accordion">
+								<ul>
+									<li style="list-style: unset;">
+										<span class="text-success font-weight-bold">
+											2 Quest Points
+										</span>
+									</li>
+									<li style="list-style: unset;">
+										<span class="text-success font-weight-bold">
+											Defence experience: {Level*300+650}
+										</span>
+									</li>
+									<li style="list-style: unset;">
+										<span class="text-success font-weight-bold">
+											Strength experience: {Level*300+650}
+										</span>
+									</li>
+									<li style="list-style: unset;">
+										<span class="text-success font-weight-bold">
+											The right to wear a Rune Plate Mail Body
+										</span>
+									</li>
+								</ul>
+							</div>
+						</div>
+
+						<div class="text-center pt-5 pb-5">
+							<a href="https://classic.runescape.wiki/w/Dragon_Slayer">Thank you to
+								the Runescape
+								Classic Wiki for
+								this quest guide!</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</article>
