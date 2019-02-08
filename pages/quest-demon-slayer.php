43efe919ade9180ce384f24e2934c531827caf54
Marwolf <cleako@gmail.com>
2019-02-07 19:41:56 -0500

    Adds demon slayer quest page

---
+++ pages/quest-demon-slayer.php
@@ -0,0 +1,167 @@
+<?php
+if (!defined('IN_SITE')) {
+	die("You do not have permission to access this file.");
+}
+?>
+
+<article class="text-info table-dark spaced-body full-width full-height">
+	<div class="container border-left border-info border-right table-wrapper-scroll-y mCustomScrollbar"
+		 data-mcs-theme="minimal">
+		<h2 class="h2 text-center text-capitalize display-3 pb-4">Demon Slayer</h2>
+		<div class="row sm-stats justify-content-center" style="text-transform: unset;">
+			<div class="text-primary">
+				<div class="row justify-content-center">
+					<div class="pl-3 pr-3 container">
+						<div class="pb-1" align="center">
+							<img class="display-glow img-fluid"
+								 src="../img/quests/Demon_Slayer_completed.png">
+						</div>
+
+						<div class="flex-row stats pl-5 pr-5">
+							<div class="sm-stats col-sm">
+								<span class="d-block"><span class="text-info">Release date:</span> 4 January 2001</span>
+								<span class="d-block"><span class="text-info">Start:</span> Talk to the Gypsy at Varrock Square</span>
+								<span class="d-block"><span class="text-info">Difficulty:</span> <span
+										class="small text-info font-weight-bold">
+									<i class="fas fa-star"></i>
+									<i class="fas fa-star"></i>
+								</span><span
+										class="small">
+									<i class="far fa-star"></i>
+									<i class="far fa-star"></i>
+									<i class="far fa-star"></i>
+									</span></span>
+								<span class="d-block"><span class="text-info">Length:</span> Medium</span>
+
+								<span class="d-block d-responsive"><span class="text-info">Items needed:</span>
+									<li>- A bucket of water (obtained during the quest)</li>
+									<li>- 25 bones</li>
+									<li>- 1 coin</li>
+									<li>- Some food and armour if you're a low level.</li>
+								</span>
+								<span class="d-block"><span
+										class="text-info">Requirements:</span> Ability to defeat a level 30 demon without assistance</span>
+								<span class="d-block"><span class="text-info">Monsters to kill:</span> Delrith (level 30)</span>
+							</div>
+						</div>
+
+						<div class="stats pl-5 pr-5" id="accordion">
+							<div class="h4 text-info">Walkthrough</div>
+							<div class="text-primary btn btn-link" data-toggle="collapse"
+								 data-target="#collapsewalkthrough"
+								 aria-expanded="false" aria-controls="collapseOne">
+								Show contents
+							</div>
+							<div id="collapsewalkthrough" class="pt-3 collapse hide" aria-labelledby="headingOne"
+								 data-parent="#accordion">
+								<img class="pb-2 img-fluid" src="../img/quests/Varrok_square.png">
+								<span class="d-block">Talk to the Gypsy in Varrock Square and have her predict your future for 1 coin. She will tell you that 150 years ago a demon named Delrith came to Varrock, but was quarantined by a hero named Wally who defeated him with a special sword named Silverlight and trapped him away. The Gypsy says that Darkwizards at the stone circle south of Varrock's are trying to resurrect him. She tells you that you are destined to kill the demon again using Silverlight which has been passed down to Sir Prysin, a knight in Varrock Palace.</span>
+								<span class="pt-3 d-block">Head north to the palace, and once inside, head west from the castle doors to find Sir Prysin walking around. Speak to him and tell him the Gypsy sent you and then tell him you need to find Silverlight. He will tell you he locked the sword away and that three keys are required to unlock the sword. However, he lost one key while the two other keys were given to other people. Sir Prysin's key was accidentally dropped in one of the Varrock Palace's drains, while the other two keys are in the possession of Captain Rovin and Traiborn the wizard, respectively.</span>
+							</div>
+						</div>
+
+						<div class="stats pl-5 pr-5" id="accordion">
+							<div class="h4 text-info">Captain Rovin's key</div>
+							<div class="text-primary btn btn-link" data-toggle="collapse" data-target="#collapsephoenix"
+								 aria-expanded="false" aria-controls="collapseOne">
+								Show contents
+							</div>
+							<div id="collapsephoenix" class="pt-3 collapse hide" aria-labelledby="headingOne"
+								 data-parent="#accordion">
+								<img class="pb-2 img-fluid" src="../img/quests/Sir_Prysin's_location.png">
+								<span class="d-block">Go to the room in the north-west corner of the palace and then go up the ladder to the 1st floor[UK]. You'll be in a small room, the guard quarters, where Captain Rovin is located. Talk to him and tell him that you have something important to tell him. Tell him there's a demon that wants to invade Varrock and that you alone are going to fight him using Silverlight. He then will give you the Silverlight Key 2.</span>
+							</div>
+						</div>
+
+						<div class="stats pl-5 pr-5" id="accordion">
+							<div class="h4 text-info">Sir Prysin's key</div>
+							<div class="text-primary btn btn-link" data-toggle="collapse"
+								 data-target="#collapseblackarm"
+								 aria-expanded="false" aria-controls="collapseOne">
+								Show contents
+							</div>
+							<div id="collapseblackarm" class="pt-3 collapse hide" aria-labelledby="headingOne"
+								 data-parent="#accordion">
+								<img class="pb-2 img-fluid" src="../img/quests/Drain_location.png">
+								<span class="d-block">On the main floor of the palace, go to the east side where the kitchen is located. Go into the north-east room and grab the bucket, then fill it at the sink. Go outside the palace to the drain outside the kitchen and use the bucket of water on it. This will push the key down into the Varrock Sewers.</span>
+								<span class="pt-3 d-block">Now leave the through its main entrance and take the east path out of the entrance. You will come to a large patch leading north; go north along it a bit until you see a manhole. Climb down it to get into the sewers. Once you have entered the Varrock Sewers, walk west then take the north path and you should find the second key sitting on the floor. Once you have picked it up (Silverlight Key 3), leave the sewers by backtracking to the ladder leading above.</span>
+								<img class="pt-2 pb-2 img-fluid" src="../img/quests/Way_to_manhole.png">
+							</div>
+						</div>
+
+						<div class="stats pl-5 pr-5" id="accordion">
+							<h4 class="text-info">Traiborn The Wizard's key</h4>
+							<div class="text-primary btn btn-link" data-toggle="collapse" data-target="#collapsefinale1"
+								 aria-expanded="false" aria-controls="collapseOne">
+								Show contents
+							</div>
+							<div id="collapsefinale1" class="pt-3 collapse hide" aria-labelledby="headingOne"
+								 data-parent="#accordion">
+								<span class="d-block">Before you go to talk to Traiborn The Wizard on the 1st floor[UK] of the Wizard's Tower, you'll need 25 regular bones. If you don't have them already, a good method is to kill the chicken in the tower's basement, or wizards around the premesis. The Wizard's Tower is located south of Draynor Village. Once you have them, talk to Traiborn The Wizard. Tell him that you want the key that Sir Prysin gave him. He won't remember being given a key, so ask him if he has any "keys knocking around" and he will tell you he sealed a key. To break that seal, he requires 25 bones. Talk to him again and he'll take all your bones, rewarding you the final key (Silverlight Key 1).</span>
+								<span class="pt-3 d-block">Now that you have all three keys, head back to Sir Prysin in Varrock, who will finally give you Silverlight.</span>
+							</div>
+						</div>
+
+						<div class="stats pl-5 pr-5" id="accordion">
+							<h4 class="text-info">Preparing to kill Delrith</h4>
+							<div class="text-primary btn btn-link" data-toggle="collapse" data-target="#collapsefinale2"
+								 aria-expanded="false" aria-controls="collapseOne">
+								Show contents
+							</div>
+							<div id="collapsefinale2" class="pt-3 collapse hide" aria-labelledby="headingOne"
+								 data-parent="#accordion">
+								<img class="pt-2 pb-2 img-fluid" src="../img/quests/Good_inventory.png">
+								<span class="d-block">Now that you have Silverlight, it is time to prepare for the fight with Delrith. If you are a low level, it is advised to bring decent food such as trout for the fight. If you are a higher level, you likely will only need some armour. Also note that the Darkwizards are aggressive, so low levels may have a hard time even getting to Delrith. Ironically, the Darkwizards are significantly stronger than Delrith, although he will be aggressive to players with a combat level of 50 or lower.</span>
+								<span class="pt-3 d-block">An example of a decent inventory would be full iron or better and the remainder of your inventory filled with food that can heal more than a third of your total Hits. Note that you cannot use a weapon other than Silverlight during the fight. Activating the Thick skin Prayer is also helpful during the fight.</span>
+							</div>
+						</div>
+
+						<div class="stats pl-5 pr-5" id="accordion">
+							<h4 class="text-info">The fight</h4>
+							<div class="text-primary btn btn-link" data-toggle="collapse" data-target="#collapsefinale3"
+								 aria-expanded="false" aria-controls="collapseOne">
+								Show contents
+							</div>
+							<div id="collapsefinale3" class="pt-3 collapse hide" aria-labelledby="headingOne"
+								 data-parent="#accordion">
+								<img class="pt-2 pb-2 img-fluid" src="../img/quests/Fight_Delrith.png">
+								<span class="d-block">Now that you're ready, head to the stone circle south of Varrock. Attack Delrith, ignoring the Darkwizards. As you fight Delrith, you'll eventually have to recite an incantation. Among the four listed options, select the bottom option, "Carlem Aber Camerinthum Purchai Gabindo". Delrith will then be absorbed by a vortex and is no longer a threat to Varrock, completing the quest.</span>
+								<img class="pt-2 pb-2 img-fluid" src="../img/quests/440px-Demon_Slayer_completed.png">
+							</div>
+						</div>
+
+						<div class="stats pl-5 pr-5" id="accordion">
+							<div class="h4 text-info">Rewards</div>
+							<div class="text-primary btn btn-link" data-toggle="collapse" data-target="#collapserewards"
+								 aria-expanded="false" aria-controls="collapseOne">
+								Show contents
+							</div>
+							<div id="collapserewards" class="pt-3 collapse hide" aria-labelledby="headingOne"
+								 data-parent="#accordion">
+								<ul>
+									<li style="list-style: unset;">
+										<span class="text-success font-weight-bold">
+											3 Quest Points
+										</span>
+									</li>
+									<li style="list-style: unset;">
+										<span class="text-success font-weight-bold">
+											Silverlight
+										</span>
+									</li>
+								</ul>
+							</div>
+						</div>
+
+						<div class="text-center pt-5 pb-5">
+							<a href="https://classic.runescape.wiki/w/Black_Knights%27_Fortress_(quest)">Thank you to
+								the Runescape
+								Classic Wiki for
+								this quest guide!</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</article>
