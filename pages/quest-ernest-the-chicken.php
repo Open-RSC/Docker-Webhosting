2da24ee3e6568bb32584529311c0a064f2b96ee4
Marwolf <cleako@gmail.com>
2019-02-07 22:21:04 -0500

    Adds ernest the chicken quest page

---
+++ pages/quest-ernest-the-chicken.php
@@ -0,0 +1,159 @@
+<?php
+if (!defined('IN_SITE')) {
+	die("You do not have permission to access this file.");
+}
+?>
+
+<article class="text-info table-dark spaced-body full-width full-height">
+	<div class="container border-left border-info border-right table-wrapper-scroll-y mCustomScrollbar"
+		 data-mcs-theme="minimal">
+		<h2 class="h2 text-center text-capitalize display-3 pb-4">Ernest The Chicken</h2>
+		<div class="row sm-stats justify-content-center" style="text-transform: unset;">
+			<div class="text-primary">
+				<div class="row justify-content-center">
+					<div class="pl-3 pr-3 container">
+						<div class="pb-1" align="center">
+							<img class="display-glow img-fluid"
+								 src="../img/quests/Ernest_Chicken_completed.png">
+						</div>
+
+						<div class="flex-row stats pl-5 pr-5">
+							<div class="sm-stats col-sm">
+								<span class="d-block"><span
+										class="text-info">Release date:</span> 21 January 2001</span>
+								<span class="d-block"><span class="text-info">Start:</span> Speak to Veronica outside Draynor Manor</span>
+								<span class="d-block"><span class="text-info">Difficulty:</span> <span
+										class="small text-info font-weight-bold">
+									<i class="fas fa-star"></i>
+								</span><span
+										class="small">
+									<i class="far fa-star"></i>
+									<i class="far fa-star"></i>
+									<i class="far fa-star"></i>
+									<i class="far fa-star"></i>
+									</span></span>
+								<span class="d-block"><span class="text-info">Length:</span> Short</span>
+
+								<span class="d-block d-responsive"><span class="text-info">Items needed: (These items are obtained during the quest)</span>
+									<li>- A spade</li>
+									<li>- Fish food</li>
+									<li>- Poison</li>
+								</span>
+								<span class="d-block d-responsive"><span class="text-info">Requirements:</span> None</span>
+								<span class="d-block d-responsive"><span class="text-info">Monsters to kill:</span> None</span>
+							</div>
+						</div>
+
+						<div class="stats pl-5 pr-5" id="accordion">
+							<div class="h4 text-info">Walkthrough</div>
+							<div class="text-primary btn btn-link" data-toggle="collapse"
+								 data-target="#collapsewalkthrough"
+								 aria-expanded="false" aria-controls="collapseOne">
+								Show contents
+							</div>
+							<div id="collapsewalkthrough" class="pt-3 collapse hide" aria-labelledby="headingOne"
+								 data-parent="#accordion">
+								<span class="d-block">To start the quest, talk to Veronica at the entrance to Draynor Manor, located to the north of Draynor Village. She explains that herself and her fiancé, Ernest, were lost. Ernest went into the mansion to get directions and he hasn't returned, and asks you to go into the mansion to find him. Note that there are Ghosts, a level 25 Witch, and a level 21 Skeleton in the manor but that they are all non-aggressive.</span>
+								<span class="pt-3 d-block">Go into the manor and go up the stairs, then go up the nearby ladder to reach the top floor. Talk to Professor Oddenstein. When you ask him about Ernest, he says that Ernest is helping him with his experiments but that he was turned into a Chicken by the professor's "pouletmorph machine."</span>
+								<span class="pt-3 d-block">He can transform him back, but his machine is no longer working and the "house gremlins" have ran off with parts needed to get it working again. He says the parts are either in the manor or on its grounds, and that he needs a Pressure Gauge, Rubber Tube and an Oil Can.</span>
+							</div>
+						</div>
+
+						<div class="stats pl-5 pr-5" id="accordion">
+							<div class="h4 text-info">Pressure Gauge</div>
+							<div class="text-primary btn btn-link" data-toggle="collapse" data-target="#collapsephoenix"
+								 aria-expanded="false" aria-controls="collapseOne">
+								Show contents
+							</div>
+							<div id="collapsephoenix" class="pt-3 collapse hide" aria-labelledby="headingOne"
+								 data-parent="#accordion">
+								<span class="d-block">Leave the Professor's room, go down the ladder and grab the Fish Food in the eastern room. Now go downstairs to the ground floor and pick up the Poison in the small western room near the Witch. Use the Poison with the Fish Food to create Poisoned Fish Food.</span>
+								<img class="pt-2 pb-2 img-fluid" src="../img/quests/Pressaure_cauge.jpg">
+								<span class="pt-3 d-block">Now go to the eastern-most room, pick up the Spade, and then head outside. Walk around to the west side of the manor. Do not approach the trees as they will do some damage based on how many Hits you have. Once you're on the west side go a little further south to the fountain. Use the Poisoned Fish Food on the fountain, which will kill some piranhas. Now search the fountain and you will obtain the Pressure Gauge.</span>
+							</div>
+						</div>
+
+						<div class="stats pl-5 pr-5" id="accordion">
+							<div class="h4 text-info">Rubber Tube</div>
+							<div class="text-primary btn btn-link" data-toggle="collapse"
+								 data-target="#collapseblackarm"
+								 aria-expanded="false" aria-controls="collapseOne">
+								Show contents
+							</div>
+							<div id="collapseblackarm" class="pt-3 collapse hide" aria-labelledby="headingOne"
+								 data-parent="#accordion">
+								<img class="pb-2 img-fluid" src="../img/quests/Key.jpg">
+								<span class="d-block">Go north of the water fountain to the Cabbage patch. Use the Spade on the nearby Compost Heap and you will obtain a Closet Key. Go back into the manor through the front entrance and use the Closet Key on the door to the small room behind the staircase leading to the second floor. Enter the closet and grab the Rubber Tube. Note that the Skeleton is non-aggressive, so you won't have to worry about him attacking you at low levels. Use the key on the door again to leave the room.</span>
+							</div>
+						</div>
+
+						<div class="stats pl-5 pr-5" id="accordion">
+							<h4 class="text-info">Oil Can</h4>
+							<div class="text-primary btn btn-link" data-toggle="collapse" data-target="#collapsefinale1"
+								 aria-expanded="false" aria-controls="collapseOne">
+								Show contents
+							</div>
+							<div id="collapsefinale1" class="pt-3 collapse hide" aria-labelledby="headingOne"
+								 data-parent="#accordion">
+								<img class="pb-2 img-fluid" src="../img/quests/600px-Basement.jpg">
+								<span class="d-block">Head to the south-western room on the main floor and go down the ladder. This room contains a puzzle solved by pulling levers. Pull the levers in the following order:</span>
+								<img class="pb-2 img-fluid" src="../img/quests/440px-ErnestTheChickenLevers.png">
+								<span class="pt-3 d-block">Pull LeverA then LeverB. Go through the north-eastern door.</span>
+								<span class="pt-3 d-block">Pull LeverD. Go through the south-western door then go through the southern door.</span>
+								<span class="pt-3 d-block">Pull LeverA then LeverB again. Go through the north-western door, then the western door, and then the northern door.</span>
+								<span class="pt-3 d-block">Pull LeverE then LeverF. Go through the eastern door and then through the other eastern door.</span>
+								<span class="pt-3 d-block">Pull LeverC. Go through the north-western door then through the western door.</span>
+								<span class="pt-3 d-block">Pull LeverE. Go through the eastern door, then the southern door, and then the other southern door.</span>
+								<span class="pt-3 d-block">Now open the western door and you can pick up the Oil Can.</span>
+							</div>
+						</div>
+
+						<div class="stats pl-5 pr-5" id="accordion">
+							<h4 class="text-info">Finale</h4>
+							<div class="text-primary btn btn-link" data-toggle="collapse" data-target="#collapsefinale3"
+								 aria-expanded="false" aria-controls="collapseOne">
+								Show contents
+							</div>
+							<div id="collapsefinale3" class="pt-3 collapse hide" aria-labelledby="headingOne"
+								 data-parent="#accordion">
+								<span class="d-block">Now head back to the top floor and talk to Professor Oddenstein. He will take all the parts from you, start up his machine, and then transform Ernest back into a human. Ernest will thank you for helping him and give you a reward, finishing the quest.</span>
+								<img class="pt-2 pb-2 img-fluid" src="../img/quests/Ernest_Chicken_completed.png">
+							</div>
+						</div>
+
+						<div class="stats pl-5 pr-5" id="accordion">
+							<div class="h4 text-info">Rewards</div>
+							<div class="text-primary btn btn-link" data-toggle="collapse" data-target="#collapserewards"
+								 aria-expanded="false" aria-controls="collapseOne">
+								Show contents
+							</div>
+							<div id="collapserewards" class="pt-3 collapse hide" aria-labelledby="headingOne"
+								 data-parent="#accordion">
+								<ul>
+									<li style="list-style: unset;">
+										<span class="text-success font-weight-bold">
+											4 Quest Points
+										</span>
+									</li>
+									<li style="list-style: unset;">
+										<div class="row">
+											<div class="pt-2 pb-2 display-glow item10"></div>
+											<span class="text-success font-weight-bold" style="padding-top: 12px;">300 coins</span>
+										</div>
+									</li>
+								</ul>
+							</div>
+						</div>
+
+						<div class="text-center pt-5 pb-5">
+							<a href="https://classic.runescape.wiki/w/Ernest_the_Chicken">Thank you to
+								the Runescape
+								Classic Wiki for
+								this quest guide!</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</article>
