9b26e4a324cc73abe5054aaa468dcdbb70a2b946
Marwolf <cleako@gmail.com>
2019-02-08 21:59:39 -0500

    Adds goblin diplomacy quest page

---
+++ pages/quest-goblin-diplomacy.php
@@ -0,0 +1,146 @@
+<?php
+if (!defined('IN_SITE')) {
+	die("You do not have permission to access this file.");
+}
+?>
+
+<article class="text-info table-dark spaced-body full-width full-height">
+	<div class="container border-left border-info border-right table-wrapper-scroll-y mCustomScrollbar"
+		 data-mcs-theme="minimal">
+		<h2 class="h2 text-center text-capitalize display-3 pb-4">Goblin Diplomacy</h2>
+		<div class="row sm-stats justify-content-center" style="text-transform: unset;">
+			<div class="text-primary">
+				<div class="row justify-content-center">
+					<div class="pl-3 pr-3 container">
+						<div class="pb-1" align="center">
+							<img class="display-glow img-fluid"
+								 src="../img/quests/Goblin_completed.png">
+						</div>
+
+						<div class="flex-row stats pl-5 pr-5">
+							<div class="sm-stats col-sm">
+								<span class="d-block"><span
+										class="text-info">Release date:</span> 08 May 2001</span>
+								<span class="d-block"><span class="text-info">Start:</span> Talk to the Bartender in Port Sarim</span>
+								<span class="d-block"><span class="text-info">Difficulty:</span> <span
+										class="small text-info font-weight-bold">
+									<i class="fas fa-star"></i>
+								</span><span
+										class="small">
+									<i class="far fa-star"></i>
+									<i class="far fa-star"></i>
+									<i class="far fa-star"></i>
+									<i class="far fa-star"></i>
+									</span></span>
+								<span class="d-block"><span class="text-info">Length:</span> Medium</span>
+
+								<span class="d-block d-responsive"><span class="text-info">Items needed: (These items are obtained during the quest)</span>
+									<li>- Three pieces of goblin armour</li>
+									<li>- Two onions or a Yellow dye (An Orange dye may replace this)</li>
+									<li>- Two woad leaves or a Blue dye</li>
+									<li>- Three redberries or a Red dye (An Orange dye may replace this)</li>
+									<li>- 35 coins</li>
+								</span>
+								<span class="d-block d-responsive"><span
+										class="text-info">Requirements:</span> None</span>
+								<span class="d-block d-responsive"><span class="text-info">Monsters to kill:</span> None</span>
+							</div>
+						</div>
+
+						<div class="stats pl-5 pr-5" id="accordion">
+							<div class="h4 text-info">Walkthrough</div>
+							<div class="text-primary btn btn-link" data-toggle="collapse"
+								 data-target="#collapsewalkthrough"
+								 aria-expanded="false" aria-controls="collapseOne">
+								Show contents
+							</div>
+							<div id="collapsewalkthrough" class="pt-3 collapse hide" aria-labelledby="headingOne"
+								 data-parent="#accordion">
+								<span class="d-block">Note that obtaining all the items before starting this quest will greatly reduce the amount of time it takes to complete it.</span>
+								<img class="pt-2 pb-2 img-fluid" src="../img/quests/Startq_goblin_diplomacy.png">
+								<span class="pt-3 d-block">Talk to the Bartender in Port Sarim. He's located in The Rusty Anchor in the building beside the fence separating Port Sarim and Draynor Village. The bartender will tell you that some Goblins are fighting about the colour of their armour in their village. Head to Goblin Village, located to the north past Falador and Doric, and talk to either General Bentnoze or General Wartface. The goblin will tell you that he would like orange-coloured armour.</span>
+								<span class="pt-3 d-block">If you don't have any Goblin Armour on you, kill some Goblins until you have three of them.</span>
+								<img class="pt-2 pb-2 img-fluid" src="../img/quests/440px-Goblingenerals.png">
+							</div>
+						</div>
+
+						<div class="stats pl-5 pr-5" id="accordion">
+							<div class="h4 text-info">Dying the armour orange</div>
+							<div class="text-primary btn btn-link" data-toggle="collapse" data-target="#collapsephoenix"
+								 aria-expanded="false" aria-controls="collapseOne">
+								Show contents
+							</div>
+							<div id="collapsephoenix" class="pt-3 collapse hide" aria-labelledby="headingOne"
+								 data-parent="#accordion">
+								<span class="d-block">o create orange dye, you'll need two onions, three redberries, and 10 coins. Onions can be obtained from the field behind Farmer Fred's house, located on the path leading from Draynor Village to Lumbridge. Redberries are located to the west of the small mine south-east of Varrock. Once you have the two onions and three redberries, visit Aggie in Draynor Village. She can be found in the house that has two red dots in it on the mini-map (representing item spawns for cheese and tomato).</span>
+								<span class="pt-3 d-block">Ask her to make you a red and yellow dye. When you have both, use one on the other to mix them to create orange dye. Now use the orange dye on the Goblin Armour. Return to Goblin Village and talk to one of the Goblin generals and they will take the orange armour. They will say they don't like the colour and want to see dark blue-coloured armour.</span>
+							</div>
+						</div>
+
+						<div class="stats pl-5 pr-5" id="accordion">
+							<div class="h4 text-info">Dying the armour dark blue</div>
+							<div class="text-primary btn btn-link" data-toggle="collapse"
+								 data-target="#collapseblackarm"
+								 aria-expanded="false" aria-controls="collapseOne">
+								Show contents
+							</div>
+							<div id="collapseblackarm" class="pt-3 collapse hide" aria-labelledby="headingOne"
+								 data-parent="#accordion">
+								<span class="d-block">Go to Falador Park and talk to Wyson The Gardener and ask about woad leaves. He offers to sell them to you for a price. Choose the highest price, 20 gp, and he'll generously give you two woad leaves.</span>
+								<span class="pt-3 d-block">Now go back to Aggie, have her create blue dye (costs two woad leaves and 5 coins), use the dye on the goblin armour, and head back to Goblin Village. Talk to one of the generals again and give them the blue armour. Talk to the goblins and they will take the blue armour. Again, they will say they don't like the colour, and instead want to see how light blue armour looks.</span>
+							</div>
+						</div>
+
+						<div class="stats pl-5 pr-5" id="accordion">
+							<h4 class="text-info">Finale</h4>
+							<div class="text-primary btn btn-link" data-toggle="collapse" data-target="#collapsefinale3"
+								 aria-expanded="false" aria-controls="collapseOne">
+								Show contents
+							</div>
+							<div id="collapsefinale3" class="pt-3 collapse hide" aria-labelledby="headingOne"
+								 data-parent="#accordion">
+								<span class="d-block">Talk to the generals with a regular Goblin armour in your inventory, which they will take from you. The two generals like it and thank you for sorting their argument, bringing the quest to an end.</span>
+								<img class="pt-2 pb-2 img-fluid" src="../img/quests/Goblin_completed.png">
+							</div>
+						</div>
+
+						<div class="stats pl-5 pr-5" id="accordion">
+							<div class="h4 text-info">Rewards</div>
+							<div class="text-primary btn btn-link" data-toggle="collapse" data-target="#collapserewards"
+								 aria-expanded="false" aria-controls="collapseOne">
+								Show contents
+							</div>
+							<div id="collapserewards" class="pt-3 collapse hide" aria-labelledby="headingOne"
+								 data-parent="#accordion">
+								<ul>
+									<li style="list-style: unset;">
+										<span class="text-success font-weight-bold">
+											5 Quest Points
+										</span>
+									</li>
+									<li style="list-style: unset;">
+										<span class="text-success font-weight-bold">
+											1 Gold bar
+										</span>
+									</li>
+									<li style="list-style: unset;">
+										<span class="text-success font-weight-bold">
+											Crafting experience: Level * 15 + 125
+										</span>
+									</li>
+								</ul>
+							</div>
+						</div>
+
+						<div class="text-center pt-5 pb-5">
+							<a href="https://classic.runescape.wiki/w/Goblin_Diplomacy">Thank you to
+								the Runescape
+								Classic Wiki for
+								this quest guide!</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</article>
