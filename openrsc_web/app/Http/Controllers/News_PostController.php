667febfc221bc9e129f39d3427f2df9fc26b0a28
Marwolf <cleako@gmail.com>
2019-04-29 13:13:57 -0400

    Adds auth protection to news where required

--- openrsc_web/app/Http/Controllers/News_PostController.php
+++ openrsc_web/app/Http/Controllers/News_PostController.php
@@ -9,6 +9,11 @@ use Illuminate\Validation\ValidationException;
 
 class News_PostController extends Controller
 {
+	public function __construct()
+	{
+		$this->middleware('auth', ['except' => ['index', 'show']]);
+	}
+
 	/**
 	 * Display a listing of the resource.
 	 *
--- openrsc_web/app/Http/Controllers/News_ResponseController.php
+++ openrsc_web/app/Http/Controllers/News_ResponseController.php
@@ -9,6 +9,11 @@ use Illuminate\Validation\ValidationException;
 
 class News_ResponseController extends Controller
 {
+	public function __construct()
+	{
+		$this->middleware('auth');
+	}
+
 	/**
 	 * Store a newly created resource in storage.
 	 *
@@ -16,21 +21,21 @@ class News_ResponseController extends Controller
 	 * @return void
 	 * @throws ValidationException
 	 */
-    public function store(Request $request)
-    {
-        $this->validate($request, [
-        	'reply' => 'required|min:15|max:500',
+	public function store(Request $request)
+	{
+		$this->validate($request, [
+			'reply' => 'required|min:15|max:500',
 			'news_post_id' => 'required|integer'
 		]);
 
-        $news_response = new news_response();
+		$news_response = new news_response();
 		$news_response->reply = $request->reply;
 
-        $news_post = news_post::findOrFail($request->news_post_id);
-        $news_post->news_responses()->save($news_response);
+		$news_post = news_post::findOrFail($request->news_post_id);
+		$news_post->news_responses()->save($news_response);
 
-        return redirect()->route('news.show', $news_post->id);
-    }
+		return redirect()->route('news.show', $news_post->id);
+	}
 
 	/**
 	 * Show the form for editing the specified resource.
@@ -38,10 +43,10 @@ class News_ResponseController extends Controller
 	 * @param int $id
 	 * @return void
 	 */
-    public function edit($id)
-    {
-        //
-    }
+	public function edit($id)
+	{
+		//
+	}
 
 	/**
 	 * Update the specified resource in storage.
@@ -50,10 +55,10 @@ class News_ResponseController extends Controller
 	 * @param int $id
 	 * @return void
 	 */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+	public function update(Request $request, $id)
+	{
+		//
+	}
 
 	/**
 	 * Remove the specified resource from storage.
@@ -61,8 +66,8 @@ class News_ResponseController extends Controller
 	 * @param int $id
 	 * @return void
 	 */
-    public function destroy($id)
-    {
-        //
-    }
+	public function destroy($id)
+	{
+		//
+	}
 }
