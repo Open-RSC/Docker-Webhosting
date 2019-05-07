31b3f110e9cdd4f279b28fa85d3ac39ed2b7b01e
Marwolf <cleako@gmail.com>
2019-05-06 23:30:58 -0400

    Adds var descriptions in comments

--- openrsc_web/app/Http/Controllers/ItemController.php
+++ openrsc_web/app/Http/Controllers/ItemController.php
@@ -15,21 +15,14 @@ class ItemController extends Controller
 
 	/**
 	 * @return Factory|View
+	 * @var $items
+	 * fetches the table row of the item in view and paginates the results
 	 */
 	public function index()
 	{
 		$items = DB::connection('openrsc')->table('openrsc_itemdef')->
 		orderBy('id', 'asc')->
 		paginate(15);
-
-		/*$items->each(function($object) {
-			$object->id;
-			$object->name;
-			$object->description;
-			$object->requiredLevel;
-			$object->requiredSkillID;
-			$object->basePrice;
-		});*/
 
 		return view('items')->with(compact('items'));
 	}
@@ -40,14 +33,17 @@ class ItemController extends Controller
 	 */
 	public function show($id)
 	{
-		// queries the item and returns a 404 error if not found in database
+		/**
+		 * @var $itemdef
+		 * queries the item and returns a 404 error if not found in database
+		 */
 		$itemdef = DB::connection('openrsc')->table('openrsc_itemdef')->find($id);
 		if (!$itemdef) abort(404);
 
 
 		/**
-		 * @var $totalPlayerHeld
-		 * determines a count for how many of the item the player base has total
+		 * @var $totalPlayerHeld_bank
+		 * determines a count for how many of the item the player base has total in their bank
 		 */
 		$totalPlayerHeld_bank = DB::connection('openrsc')->
 		table('openrsc_bank')->
@@ -62,6 +58,11 @@ class ItemController extends Controller
 		})->
 		sum('amount');
 
+
+		/**
+		 * @var $totalPlayerHeld_invitems
+		 * determines a count for how many of the item the player base has total in their inventory
+		 */
 		$totalPlayerHeld_invitems = DB::connection('openrsc')->
 		table('openrsc_invitems')->
 		select('openrsc_invitems.id', 'openrsc_invitems.playerID', 'openrsc_invitems.amount', 'openrsc_players.id', 'openrsc_players.group_id', 'openrsc_players.banned')->
@@ -75,14 +76,18 @@ class ItemController extends Controller
 		})->
 		sum('amount');
 
+
+		/**
+		 * @var $totalPlayerHeld
+		 * determines a count for how many of the item the player base has total in their bank and inventory combined
+		 */
 		$totalPlayerHeld = $totalPlayerHeld_invitems + $totalPlayerHeld_bank;
 
 
 		/**
-		 * @var $last3moPlayerHeld
-		 * determines a count for how many of the item the player base has for those active within the last 3 months
+		 * @var $last3moPlayerHeld_bank
+		 * determines a count for how many of the item the player base has for those active within the last 3 months in their bank
 		 */
-
 		$last3moPlayerHeld_bank = DB::connection('openrsc')
 			->table('openrsc_bank')
 			->select('openrsc_bank.id', 'openrsc_bank.playerID', 'openrsc_bank.amount', 'openrsc_players.id', 'openrsc_players.group_id', 'openrsc_players.banned', 'openrsc_players.login_date')
@@ -97,6 +102,11 @@ class ItemController extends Controller
 			})
 			->sum('amount');
 
+
+		/**
+		 * @var $last3moPlayerHeld_invitems
+		 * determines a count for how many of the item the player base has for those active within the last 3 months in their inventory
+		 */
 		$last3moPlayerHeld_invitems = DB::connection('openrsc')
 			->table('openrsc_invitems')
 			->select('openrsc_invitems.id', 'openrsc_invitems.playerID', 'openrsc_invitems.amount', 'openrsc_players.id', 'openrsc_players.group_id', 'openrsc_players.banned', 'openrsc_players.login_date')
@@ -111,12 +121,17 @@ class ItemController extends Controller
 			})
 			->sum('amount');
 
+
+		/**
+		 * @var $last3moPlayerHeld
+		 * determines a count for how many of the item the player base has total in their bank and inventory combined for players that have logged in within the last 3 months
+		 */
 		$last3moPlayerHeld = $last3moPlayerHeld_invitems + $last3moPlayerHeld_bank;
 
 
 		/**
 		 * @var $item_drops
-		 * gathers a list of the npcs and their associated drop tables
+		 * gathers a list of the npcs and their associated drop tables, then paginates the table
 		 */
 		$item_drops = DB::connection('openrsc')
 			->table('openrsc_npcdrops AS B')
