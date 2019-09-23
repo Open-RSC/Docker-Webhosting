7957ec6f7eeec491a4d6d0b1d1971b28011b1364
Marwolf <marwood.wolf@gmail.com>
2019-09-23 18:39:56 -0400

    added mouseover coords for worldmap

--- openrsc_web/resources/views/worldmap.blade.php
+++ openrsc_web/resources/views/worldmap.blade.php
@@ -18,19 +18,33 @@
 	<div class="text-center" style="overflow: auto;">
 		<script>
             function drawPosition() {
-                const c = document.getElementById('canvas');
-                if (c.getContext) {
-                    const ctx = c.getContext('2d');
-                    const player = new Image();
-					{{ implode('', $areaPlayer) }}
-                }
+                let canvas = document.getElementById('canvas');
+                let context = canvas.getContext('2d');
+
+                context.font= '16px Arial';
+                context.textBaseline = 'middle';
+                context.fillStyle = '#000000';
+
+                canvas.addEventListener('mouseout', function(e) {
+                    context.clearRect(0, 0, canvas.width, canvas.height);
+                });
+
+                canvas.addEventListener('mousemove', function(e) {
+                    context.clearRect(0, 0, canvas.width, canvas.height);
+                    context.beginPath();
+                    context.arc(e.layerX, e.layerY, 3, 0, 2 * Math.PI, false);
+                    context.closePath();
+                    context.fill();
+                    let realx = Math.round((-e.offsetX/3 + 767));
+                    let realy = Math.round(e.layerY/3 + 433);
+                    let text = '(' + realx + ', ' + realy + ')';
+                    context.fillText(text, e.layerX + 5, e.layerY);
+                });
             }
 		</script>
 
 		<canvas style="background-image: url({{ asset('img/worldmap.png') }}" id="canvas" width="2152" height="1007">
-			<script>
-                drawPosition();
-			</script>
+			<script>drawPosition();</script>
 		</canvas>
 	</div>
 @endsection
