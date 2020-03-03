09308c571fa156f21a6f22b4761e11bf6cde3824
Marwolf <marwolf@openrsc.com>
2020-03-03 16:03:09 -0500

    Sets up templateborder

---
+++ openrsc_web/resources/views/includes/borderbottom.blade.php
@@ -0,0 +1,21 @@
+<table class="mx-auto" style="border-spacing: 0">
+	<tr>
+		<td class="align-bottom" style="padding: 0">
+			<img src="{{ asset('images/edge_g2.jpg') }}" alt="edge">
+		</td>
+		<td class="align-bottom" style="padding: 0">
+			<img src="{{ asset('images/edge_c.jpg') }}" alt="edge">
+		</td>
+		<td class="align-bottom" style="padding: 0">
+			<img src="{{ asset('images/edge_h2.jpg') }}" alt="edge">
+		</td>
+	</tr>
+</table>
+</td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+</div>
+</div>
--- openrsc_web/resources/views/play.blade.php
+++ openrsc_web/resources/views/play.blade.php
@@ -1,74 +1,7 @@
-@extends('template')
-
+@extends('templateborder')
 @section('content')
 
-	<div class="justify-content-center row text-center">
+	<img src="{{ asset('images/cabbage_logo.png') }}" height="300px" width="300px"
+		 alt="alt">
 
-		<div class="pt-4 pb-3">
-			<table class="table-responsive">
-				<tr>
-					<td class="align-middle">
-						<table style="border-spacing: 0">
-							<tr>
-								<td class="align-top" style="padding: 0">
-									<img src="{{ asset('images/edge_a.jpg') }}" alt="edge">
-								</td>
-								<td class="align-top" style="padding: 0">
-									<img src="{{ asset('images/edge_c.jpg') }}" alt="edge">
-								</td>
-								<td class="align-top" style="padding: 0">
-									<img src="{{ asset('images/edge_d.jpg') }}" alt="edge">
-								</td>
-							</tr>
-						</table>
-						<table class="mx-auto"
-							   style="background-image: url({{ asset('images/background2.jpg') }}); border-spacing: 0">
-							<tr>
-								<td style="padding: 0">
-									<table class="mx-auto"
-										   style="width: 250px; background-color: black; border-spacing: 0">
-										<tr>
-											<td class="e" style="padding: 0">
-												<span class="d-block font_weight_bold">
-													Play RuneScape
-												</span>
-												<span class="d-block">
-													<a class="c" href="{{ asset('home') }}">
-														Main Menu
-													</a>
-													-
-													<a class="c" href="detail.html">
-														Detail Select
-													</a>
-												</span>
-											</td>
-										</tr>
-									</table>
-									<!-- Begin content -->
-
-									<img src="{{ asset('images/cabbage_logo.png') }}" height="300px" width="300px"
-										 alt="alt">
-
-									<!-- End content -->
-									<table class="mx-auto" style="border-spacing: 0">
-										<tr>
-											<td class="align-bottom" style="padding: 0">
-												<img src="{{ asset('images/edge_g2.jpg') }}" alt="edge">
-											</td>
-											<td class="align-bottom" style="padding: 0">
-												<img src="{{ asset('images/edge_c.jpg') }}" alt="edge">
-											</td>
-											<td class="align-bottom" style="padding: 0">
-												<img src="{{ asset('images/edge_h2.jpg') }}" alt="edge">
-											</td>
-										</tr>
-									</table>
-								</td>
-							</tr>
-						</table>
-					</td>
-				</tr>
-			</table>
-		</div>
-	</div>
 @endsection
