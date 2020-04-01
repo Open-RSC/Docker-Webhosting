27a8373e200d22df68afde8c7b53df37f2c49161
Marwolf <marwolf@openrsc.com>
2020-04-01 01:04:46 -0400

    Adds custom error pages

--- openrsc_web/resources/views/errors/403.blade.php
+++ openrsc_web/resources/views/errors/403.blade.php
@@ -1,5 +1,25 @@
-@extends('errors::minimal')
+@extends('templateborder')
+@section('content')
 
-@section('title', __('Forbidden'))
-@section('code', '403')
-@section('message', __($exception->getMessage() ?: 'Forbidden'))
+	<header class="rsc-box rsc-header">
+		<span class="d-block">Open RuneScape Classic</span>
+		<a class="rsc-link" href="/">Main Menu</a>
+	</header>
+
+	<div class="pt-1">
+		<div class="rsc-box rsc-hiscores-ranks text-center">
+			<!-- Full version -->
+			<div class="d-none d-md-block d-lg-block d-xl-block">
+				<img class="d-block pb-1" src="{{ asset('images/apocalypse.png') }}" style="width: 720px" alt="404"/>
+			</div>
+
+			<!-- Mobile version -->
+			<div class="d-sm d-md-none d-lg-none">
+				<img class="d-block" src="{{ asset('images/apocalypse.png') }}" style="width: 288px" alt="404"/>
+			</div>
+
+			<span class="h3">403 - Forbidden</span>
+		</div>
+	</div>
+
+@endsection
