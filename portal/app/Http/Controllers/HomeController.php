a5825fe8215b90bc333fa17c527ff1aaa36b9535
Marwolf <marwolf@openrsc.com>
2020-04-12 22:10:19 -0400

    Portal complete

--- portal/app/Http/Controllers/HomeController.php
+++ portal/app/Http/Controllers/HomeController.php
@@ -10,4 +10,9 @@ class HomeController extends Controller
     {
         return view('home');
     }
+
+    public function play()
+    {
+        return view('play');
+    }
 }
--- portal/routes/web.php
+++ portal/routes/web.php
@@ -14,3 +14,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'HomeController@index')->name('home');
+Route::get('/play', 'HomeController@play')->name('play');
